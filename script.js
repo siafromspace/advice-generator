f2da5c6889790a5b66b775f6e1a90965e9b5cd39
siafromspace <sharonyak15@gmail.com>
2022-06-30 19:25:30 +0100

    advice generator

---
+++ script.js
@@ -0,0 +1,16 @@
+const adviceNo = document.getElementById("advice-no")
+const adviceText = document.getElementById("advice-text")
+const dice = document.querySelector(".click-dice")
+
+function getQuote() {
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            adviceNo.innerText = `Advice #${data.slip.id}`
+            adviceText.innerText = `"${data.slip.advice}"`
+        })
+}
+window.onload = getQuote
+dice.addEventListener("click", getQuote)
